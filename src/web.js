f008b359d6e39d64350404fd1ce1b8218ad3bcf7
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-02-15 13:32:06 +0100

    Update dexpress

--- src/web.js
+++ src/web.js
@@ -1,6 +1,6 @@
 require('dotenv').config({ silent: true });
 
-const app = require('dexpress')();
+const app = require('dexpress').app;
 app.use(require('body-parser').json());
 
 const skill = new (require('dalexa').Skill)();
