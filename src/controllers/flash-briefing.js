c26cabebb3e1458101578dbbd3f8f59d88b2a517
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-02-15 13:32:18 +0100

    Add flash briefing example

---
+++ src/controllers/flash-briefing.js
@@ -0,0 +1,9 @@
+module.exports = ['get', ['/', async (req, res) => {
+  res.send({
+    uid: 'uid',
+    updateDate: 'updateDate',
+    titleText: 'titleText',
+    mainText: 'mainText',
+    redirectionUrl: 'redirectionUrl',
+  });
+}]];
--- src/web.js
+++ src/web.js
@@ -3,6 +3,10 @@ require('dotenv').config({ silent: true });
 const app = require('dexpress').app;
 app.use(require('body-parser').json());
 
+require('dcontrollers')(app, [
+  require('./controllers/flash-briefing'),
+]);
+
 const skill = new (require('dalexa').Skill)();
 skill.onIntents([
   require('./intents/hello'),
